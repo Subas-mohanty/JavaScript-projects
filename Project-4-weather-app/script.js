58f1ca44bebf27b834457df9ad8f0e7464a8ed62
subas <subasmohanty256@gmail.com>
2023-11-21 23:01:35 +0530

    handling the error

--- Project-4-weather-app/script.js
+++ Project-4-weather-app/script.js
@@ -1,24 +1,28 @@
 let cityName=document.getElementById("city");
 const btnElement=document.querySelector("button");
+async function getWeather(cityName) {
+    try {
+        const apiKey = `https://api.weatherapi.com/v1/current.json?key=191bf9d0b46543dc9da173843232808&q=${cityName}&aqi=yes`;
+        const api = await fetch(apiKey);
 
-async function getWeather (cityName){
-    const apiKey=`https://api.weatherapi.com/v1/current.json?key=191bf9d0b46543dc9da173843232808&q=${cityName}&aqi=yes`;
-    const api= await fetch(apiKey);
-    const data= await api.json();
+        if (!api.ok) {
+            throw new Error(`Failed to fetch weather data: ${api.status}`);
+        }
 
-    // console.log(data);
+        const data = await api.json();
 
-    // document.querySelector(".cityName").innerHTML=data.location.name;
-    // document.getElementById("cityName").innerHTML=data.location.name;
-    document.getElementsByClassName("cityName")[0].innerHTML=data.location.name;
-    document.getElementsByClassName("temperature")[0].innerHTML=data.current.temp_c + "°C"; 
-    document.getElementsByClassName("humidity")[0].innerHTML=data.current.humidity + "%";
-    document.getElementsByClassName("wind")[0].innerHTML=data.current.wind_kph + " km/h";
+        document.getElementsByClassName("cityName")[0].innerHTML = data.location.name;
+        document.getElementsByClassName("temperature")[0].innerHTML = data.current.temp_c + "°C";
+        document.getElementsByClassName("humidity")[0].innerHTML = data.current.humidity + "%";
+        document.getElementsByClassName("wind")[0].innerHTML = data.current.wind_kph + " km/h";
 
-    localStorage.setItem("city",`${cityName}`);
-    localStorage.setItem("humidity",`${data.current.humidity}`);
-    localStorage.setItem("temp",`${data.current.temp_c }`);
-    localStorage.setItem("wind",`${data.current.wind_kph }`);
+        localStorage.setItem("city", `${cityName}`);
+        localStorage.setItem("humidity", `${data.current.humidity}`);
+        localStorage.setItem("temp", `${data.current.temp_c}`);
+        localStorage.setItem("wind", `${data.current.wind_kph}`);
+    } catch (error) {
+        console.error("Error fetching weather data:", error);
+    }
 }
 btnElement.addEventListener("click",async ()=>{
     getWeather(cityName.value)
